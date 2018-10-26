c4fa220356041cf5ae78bcd9a0002880932c87ee
Schamberger12 <32084866+Schamberger12@users.noreply.github.com>
2018-10-26 15:47:08 -0500

    Paint Between Dots

--- Assets/Scripts/StickBehavior.cs
+++ Assets/Scripts/StickBehavior.cs
@@ -21,6 +21,9 @@ public class StickBehavior : MonoBehaviour {
 	private bool drill = false; // not supported yet
 	private PaintableTexture pt;
 	private LayerMask layerMask = Physics.DefaultRaycastLayers;
+
+    private Vector2 paint1;
+    private Vector2 paint2;
 	
 	void Start () {
 		rends = gameObject.GetComponentsInChildren<Renderer>();
@@ -79,17 +82,22 @@ public class StickBehavior : MonoBehaviour {
 
 	void Update () {
 
+
+
         OVRInput.Update();
 
         transform.localPosition = OVRInput.GetLocalControllerPosition(Controller);
         transform.localRotation = OVRInput.GetLocalControllerRotation(Controller) * Quaternion.Euler(90, 0, 0);
+
+        this.paint1 = this.paint2;
 
 
         if (isDrawing) {
 			if (drill) {
 				PaintAllHits();
 			} else {
-				PaintFirstHit();
+                GameObject g = PaintFirstHit();
+                paintBetweenPoints(g);
 			}
 		}
 	}
@@ -99,7 +107,41 @@ public class StickBehavior : MonoBehaviour {
 		PaintFirstHit();
 	}
 
-    void PaintFirstHit() {
+    void paintBetweenPoints(GameObject g)
+    {
+
+        //get the slope between x & y
+        float M = (this.paint2.y - this.paint1.y) / (this.paint2.x - this.paint1.x);
+        //absolute value so it doesn't matter which is greater
+        Mathf.Abs(M);
+        float b = 0.0f;
+
+        if (paint1.x >= paint2.x)
+        {
+            b = paint2.x;
+        }
+        else if(paint2.x > paint1.x)
+        {
+            b = paint1.x;
+        }
+
+        while (Vector2.Distance(this.paint1, paint2) != 0.0f)
+        {
+            
+            if(this.paint2.x > this.paint1.x)
+            {
+                paint1.Set(paint1.x + 0.001f, paint1.x * M + b);
+                pt.PaintUV(g, paint1);
+            }
+            else
+            {
+                paint2.Set(paint2.x + 0.001f, paint2.x * M + b);
+                pt.PaintUV(g, paint2);
+            }
+        }
+    }
+
+    GameObject PaintFirstHit() {
 		// Local "up"
         var raydir = transform.TransformDirection(Vector3.up);
         RaycastHit hit;
@@ -112,7 +154,11 @@ public class StickBehavior : MonoBehaviour {
 				// Paint on the shared PaintableTexture at the (u,v) coordinates
 				// of the point where the ray met the object.
 				pt.PaintUV (g, hit.textureCoord);
+                this.paint2 = hit.textureCoord;
+                return g;
 			}
+            return null;
 		}
+        return null;
 	}
 }

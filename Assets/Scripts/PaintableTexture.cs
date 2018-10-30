9fb92e86069d64cc5139734224a78b20f4b803b5
David Dumas <david@dumas.io>
2018-10-30 15:01:03 -0500

    Prevent trivial .mat file changes
    Previously, after running genuslab in the editor, there would often be
    small changes to g2paintmaterial.mat and h2paintmaterial.mat, because
    setting the paint coordinates in PaintUV() of PaintableTexture.cs
    resulted in those coordinates being saved to disk.
    
    Now we make a copy of each paint material in
    ReplaceTextureWithRenderTexture() of PaintableTexture.cs and use that
    copy for all paint operations.  Thus the changes are discarded when the
    application exits, and never written to disk.

--- Assets/Scripts/PaintableTexture.cs
+++ Assets/Scripts/PaintableTexture.cs
@@ -89,7 +89,9 @@ public class PaintableTexture : MonoBehaviour {
 				mcopy.SetTexture(mainTexturePropertyID, rt);
                 PaintData pd = go.GetComponent<PaintData>();
                 if (pd != null ) {
-                    paintMaterials[go] = pd.paintMaterial;
+                    // We duplicate the paintMaterial specified in the scene so that
+                    // changes to its properties (as in PaintUV) do not get written to disk.
+                    paintMaterials[go] = new Material(pd.paintMaterial);
                 } else {
                     paintMaterials[go] = null;
                 }

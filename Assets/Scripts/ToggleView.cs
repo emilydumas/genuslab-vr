5b87e30a3a4553a022e9738cecc4b4ea2b81f105
Adrahtas Alexander <aadrah2@AD.MCL.MATH.UIC.EDU>
2018-11-27 15:54:36 -0600

    Added Button Materials

---
+++ Assets/Scripts/ToggleView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleView : MonoBehaviour {
+	public List<Material> options;
+	int current=1;
+	private Renderer r; 
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void Update(){
+		GetComponent<Renderer>().material=options[current];
+	}
+	void OnTriggerEnter(){
+		NextTexture();
+	}
+	public void SetTexture(int idx) {
+        current = idx;
+    }
+
+    public void NextTexture() {
+        SetTexture((current + 1) % options.Count);
+    }
+    public void PreviousTexture() {
+        if (current > 0) {
+            SetTexture(current - 1);
+        } else {
+            SetTexture(options.Count-1);
+        }
+    }
+}

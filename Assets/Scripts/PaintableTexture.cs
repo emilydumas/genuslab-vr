363348fed9c7780bbe38e79f42860771c76f3f2e
alx168 <alx168@gmail.com>
2018-11-30 18:49:42 -0600

    merged erasure tool but needs fixes

--- Assets/Scripts/PaintableTexture.cs
+++ Assets/Scripts/PaintableTexture.cs
@@ -97,21 +97,21 @@ public class PaintableTexture : MonoBehaviour {
                     paintMaterials[go] = null;
                 }
             }
-        }
-        // It is possible that the object we're inspecting has a ssoitControl script attached,
+            // It is possible that the object we're inspecting has a ssoitControl script attached,
             // but Start() has not run on that script, so the SSOIT material hasn't been applied
             // yet.  That means we would miss a place where we should apply the RenderTexture.
             // To fix this, we manually check for such a script, and if necessary, set the main
             // texture on its ssoitMaterial to the RenderTexture.
             // TODO: Find a better solution to this race condition.
-        ssoitControl SC = go.GetComponent<ssoitControl>();
-            if (SC != null) {
-                if (SC.ssoitMaterial.GetTexture(mainTexturePropertyID) == t) {
-                    Material mcopy = new Material(SC.ssoitMaterial);
-                    mcopy.SetTexture(mainTexturePropertyID,rt);
-                    SC.ssoitMaterial = mcopy;
+            ssoitControl SC = go.GetComponent<ssoitControl>();
+                if (SC != null) {
+                    if (SC.ssoitMaterial.GetTexture(mainTexturePropertyID) == t) {
+                        Material mcopy = new Material(SC.ssoitMaterial);
+                        mcopy.SetTexture(mainTexturePropertyID,rt);
+                        SC.ssoitMaterial = mcopy;
+                    }
                 }
-            }
+        }
     }
     public void PaintUV(GameObject obj, Vector2 uv) {
 		// Draw a new spot on the rendertexture at (u,v)
@@ -129,7 +129,7 @@ public class PaintableTexture : MonoBehaviour {
     public void Clear() {
         Graphics.Blit(target,rt);
 
-		Graphics.Blit(options[currentTextureOption],rt);
+		//Graphics.Blit(options[currentTextureOption],rt);
 	}
 
     public void SetTexture(int idx) {

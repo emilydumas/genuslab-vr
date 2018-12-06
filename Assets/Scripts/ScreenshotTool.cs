5de4ca15aabf1861c68334600a42ae452db810b6
David Dumas <david@dumas.io>
2018-12-06 13:49:17 -0600

    Add screenshot tool
    Press control-alt-S to toggle (starts off).
    
    Press Y on left touch controller, or alt-S, to save a screenshot.
    
    The default destination for binary builds is the GenusLabVR_Data
    directory.

---
+++ Assets/Scripts/ScreenshotTool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotTool : MonoBehaviour {	
+	public bool allowScreenshots = false;
+
+	void toggle() {
+		allowScreenshots = !allowScreenshots;
+	}
+
+	void Update () {
+		if ((Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl)) && ((Input.GetKey(KeyCode.RightAlt) || Input.GetKey(KeyCode.LeftAlt))  && Input.GetKeyDown(KeyCode.S))) {
+			toggle();
+		}
+
+		if (allowScreenshots) {
+			if (OVRInput.GetDown(OVRInput.RawButton.Y) || ((Input.GetKey(KeyCode.RightAlt) || Input.GetKey(KeyCode.LeftAlt))  && Input.GetKeyDown(KeyCode.S))) {
+				// Take a screenshot
+				DateTime dt = DateTime.Now;
+				ScreenCapture.CaptureScreenshot(Application.productName + " " + dt.ToString("yyyy-MM-dd THH.mm.ss") + ".png", 4);
+			}
+		}
+	}
+}
